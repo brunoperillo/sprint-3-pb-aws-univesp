bf90188afbb13e85199a32894987bd70c3d7cc0b
Bernardo Lima <bernardo.lima.93@hotmail.com>
2023-03-18 10:45:03 -0300

    :bricks: Correção da path para sua exec correta

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 036531232259.dkr.ecr.us-east-1.amazonaws.com
 
-docker build -t sprint3-compass -f # "inserir o nome do arquivo dockerfile que vai subir aplicação" .
+docker build -t sprint3-compass -f Dockerfile .
 
 docker tag sprint3-compass:latest 036531232259.dkr.ecr.us-east-1.amazonaws.com/sprint3-compass:latest
 
