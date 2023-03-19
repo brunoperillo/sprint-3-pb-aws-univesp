f6bafcbd81ed38d28a8c33ca3f15ebe69064fd04
Márcio Brunellihttps://github.com/Compass-pb-aws-2023-Univesp/sprint-1-pb-aws-univesp.git <marciolimabrunelli@gmail.com>
2023-03-19 11:09:25 -0300

    Dockerfiles running

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
+const PORT = 9000;
+const HOST = '0.0.0.0';
 
-app.use('/public', express.static(__dirname + '/public'))
+app.use('/scr', express.static(__dirname + '/scr'))
 
 app.get('/', (req, res) => {
     // envia o arquivo da página principal
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/scr/index.html')
 })
 
 app.listen(9000, () => {
-  console.log('Server online')
+  console.log(`Running on http://${HOST}:${PORT}`)
 })
