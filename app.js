6c5025df2e0717388c797c4a4571794c0a8d9e33
Diegox0301 <96358027+Diegox0301@users.noreply.github.com>
2023-03-20 11:09:26 -0300

    :lipstick: feat: Subindo novo Docker

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express();
 
-const port = 3000
+const port = 9000
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/src/index.html')
